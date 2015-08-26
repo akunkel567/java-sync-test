b0c72037468b02311ce3829bd0f122aca2ce31b6
a.kunkel <a.kunkel@9a8fb529-06f0-4ef7-967a-ea8f9ee993bd>
2015-08-26 14:16:40 +0000

    Anpassungen an Pom und Profilen wegen Umzug der Test-Schnittstellen von vKunkel auf vMiddleware
    git-svn-id: https://complex-it.xp-dev.com/svn/clxProduct/Sync/clxProductSync/branches/Version-1.0@617 9a8fb529-06f0-4ef7-967a-ea8f9ee993bd

---
+++ buildAll_sandbox.bat
@@ -0,0 +1,3 @@
+call c:\tools\maven\apache-maven-3.0.5\bin\mvn.bat -Pdaiber-sandbox clean install
+call c:\tools\maven\apache-maven-3.0.5\bin\mvn.bat -Pfare-sandbox clean install
+
---
+++ buildAll_test.bat
@@ -0,0 +1,4 @@
+call c:\tools\maven\apache-maven-3.0.5\bin\mvn.bat -Pdaiber-test clean install
+call c:\tools\maven\apache-maven-3.0.5\bin\mvn.bat -Pfare-test clean install
+call c:\tools\maven\apache-maven-3.0.5\bin\mvn.bat -Pcomplex-test clean install
+
