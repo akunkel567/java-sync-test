a66026105630e930a4af024b2f3ee471acf3b733
a.kunkel <a.kunkel@9a8fb529-06f0-4ef7-967a-ea8f9ee993bd>
2015-09-01 08:06:29 +0000

    Merged revision(s) 486-617 from Sync/clxProductSync/branches/Version-1.0: Übertragung Artgroessezulauf und Massgruppensprachen ........ Übertragung des Basispreises aus aktueller Saison mit der Übertragung der Artpreistyp-Tabelle ........ Neues ActiveRecort-Config für Artpreistyp, Übertragung des Basispreises aus aktueller Saison mit der Übertragung der Artpreistyp-Tabelle ........ Anpassungen an Pom und Profilen wegen Umzug der Test-Schnittstellen von vKunkel auf vMiddleware ........
    git-svn-id: https://complex-it.xp-dev.com/svn/clxProduct/Sync/clxProductSync/trunk@622 9a8fb529-06f0-4ef7-967a-ea8f9ee993bd

---
+++ buildAll.bat
@@ -0,0 +1,4 @@
+call c:\tools\maven\apache-maven-3.0.5\bin\mvn.bat -Pdaiber-prod clean install
+call c:\tools\maven\apache-maven-3.0.5\bin\mvn.bat -Pfare-prod clean install
+call c:\tools\maven\apache-maven-3.0.5\bin\mvn.bat -Pmfc24 clean install
+
